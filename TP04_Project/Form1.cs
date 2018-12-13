3fbdfbe35eeeb93bb16cbd6ff87af46b84ee720e
R-Men <huser.gaetan@hotmail.com>
2018-12-13 15:09:07 +0100

    Load and Save btn

--- TP04_Project/Form1.cs
+++ TP04_Project/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using ImageEdgeDetection;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,9 @@ namespace TP04_Project
 {
     public partial class ImageDetectionTP04 : Form
     {
+        private Bitmap originalImageFromFile = null;
+        private Bitmap loadedImage = null;
+
         public ImageDetectionTP04()
         {
             InitializeComponent();
@@ -19,12 +25,54 @@ namespace TP04_Project
 
         private void BtnLoad_Click(object sender, EventArgs e)
         {
+            OpenFileDialog filedialog = new OpenFileDialog();
+            filedialog.Title = "Slect an image file";
+            filedialog.Filter = "Images | *.jpg; *.jpeg; *.bmp; *.png";
+
+            if (filedialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader streamReader = new StreamReader(filedialog.FileName);
+                originalImageFromFile = (Bitmap)Image.FromStream(streamReader.BaseStream);
+                streamReader.Close();
+
+                LoadImageFromFile();
+            }
 
         }
 
         private void BtnSave_Click(object sender, EventArgs e)
         {
+            if (loadedImage != null)
+            {
+                SaveFileDialog filedialog = new SaveFileDialog();
+                filedialog.Title = "Location to save file";
+                filedialog.Filter = "Images | *.jpg; *.jpeg; *.bmp; *.png";
 
+                if (filedialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    String fileExtension = Path.GetExtension(filedialog.FileName).ToUpper();
+                    ImageFormat imgFormat;
+
+                    switch (fileExtension)
+                    {
+                        case "BMP":
+                            imgFormat = ImageFormat.Bmp;
+                            break;
+                        case "JPG":
+                            imgFormat = ImageFormat.Jpeg;
+                            break;
+                        case "PNG":
+                        default:
+                            imgFormat = ImageFormat.Png;
+                            break;
+                    }
+
+                    StreamWriter streamWriter = new StreamWriter(filedialog.FileName, false);
+                    loadedImage.Save(streamWriter.BaseStream, imgFormat);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+            }
         }
 
 
@@ -42,6 +90,22 @@ namespace TP04_Project
         private void PictureBoxForImageLoaded_Click(object sender, EventArgs e)
         {
 
+        }
+
+
+
+        private void LoadImageFromFile()
+        {
+            //comboBoxEdge.SelectedIndex = 0;
+            //comboBoxFilter.SelectedIndex = 0;
+          
+            loadedImage = originalImageFromFile.CopyToSquareCanvas(pictureBoxForImageLoaded.Width);
+            ApplyPreview();
+        }
+
+        private void ApplyPreview()
+        {
+            pictureBoxForImageLoaded.Image = loadedImage;
         }
     }
 }
